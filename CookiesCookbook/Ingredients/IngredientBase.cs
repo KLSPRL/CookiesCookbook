6929deb8bce5cd626e7a34854d1b339bf8140d03
KLSPRL <klaus.prelle@gmail.com>
2026-03-05 14:39:40 +0100

    Cookies Cookbook init

---
+++ CookiesCookbook/Ingredients/IngredientBase.cs
@@ -0,0 +1,9 @@
+﻿namespace CookiesCookbook.Ingredients
+{
+    public abstract class IngredientBase
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+        public abstract string PreparationInstructions { get; }
+    }
+}
